91b3c5704b20fbd5fde552483037b41d12e153ef
murphian <murphianx@gmail.com>
2018-12-26 15:08:10 +0800

    取消负数功能，简化逻辑

--- ex8/ex8.py
+++ ex8/ex8.py
@@ -1,3 +1,5 @@
+# conding:utf-8
+
 import sys
 import argparse
 
@@ -9,24 +11,28 @@ def argv():
     parser.add_argument('-byte', help='cut by byte.')
     parser.add_argument('-field', help='cut by field.')
     parser.add_argument('-divide', help='define the separator.')
-    parser.add_argument('content',help='the content or the file to be cut.')
+    parser.add_argument(
+        'content',
+        help='the content or the file to be cut.',
+        nargs='?'
+    )
     return parser.parse_args()
 
 def get_lines(content):
+
     if content:
         content_n = open(content).readlines()
-        the_content = []
-
-        for line in content_n:
-            if line != '\n':
-                the_line = line[:-1]
-                the_content.append(the_line)
-        return the_content
     else:
-        the_content = sys.stdin
-        print(the_content, '\n', '*'*10) ##########
-        print('*'*20, list(the_content))
-        return list(the_content)
+        content_n = list(sys.stdin)
+
+    the_content = []
+
+    for line in content_n:
+        if line != '\n':
+            the_line = line[:-1]
+            the_content.append(the_line)
+
+    return the_content
 
 def get_settings(character, byte, field, divide):
     if character:
@@ -55,90 +61,53 @@ def cut_line_to_pieces(line, divide):
     else:
         return pieces
 
-def slice_add_one(end_num):
-    # end_num 需要在切片时完成 +1 操作
-    # 特别地：当 end_num == -1 时，不作 +1 ，而是直接输出 None
-    if end_num == -1 or end_num == None:
-        return None
-    else:
-        return end_num + 1
-
-def num_guarantee(array, num):
-    # 保证 num 在 array 的index范围内
-    array_len = len(array)
-    if num + 1 > array_len:
-        return 'too big'   
-    elif num < -array_len: 
-        return 'too small'
-    else:
-        return num
-
-def start_end_num_in_range(array, start_num, end_num):
-    g_start = num_guarantee(array, start_num)
-    g_end = num_guarantee(array, end_num)
-
-    if g_start == 'too big':
-        r_start = start_num # 将输出空
-    elif g_start == 'too small':
-        r_start = None # 将从第一个开始输出
-    else:
-        r_start = start_num # 正常输出
-
-    if g_end == 'too big':
-        r_end = None # 将从最后一个开始输出
-    elif g_end == 'too small':
-        r_end = end_num # 将输出空
-    else:
-        r_end = end_num # 正常输出
-
-    # if r_start and r_end and (r_start > r_end): ####
-    #     print('ERROR: start_num > end_num')
-    #     sys.exit(1)
-
-    return r_start, r_end
-
 def processing_demand(demand):
     if not ('-' in demand):
         print('ERROR: Wrong Condition!(e.g: 2-7)')
         sys.exit(1)
 
     demand_list = demand.split('-')
+    print('demand_list: ', demand_list)
     demand_start = demand_list[0]
     demand_end = demand_list[1]
 
     if demand_start == '':
-        demand_start = str(0 + 1)
-    if demand_end == '':
-        demand_end = str(-1 + 1)
-
-    if not (demand_start.isdigit() or demand_end.isdigit()):
+        demand_start = None
+    elif not demand_start.isdigit():
         print('ERROR: Wrong Condition!(e.g: 2-7)')
         sys.exit(1)
     else:
-        start_num = int(demand_start) - 1 # index 从 0 计数，故 -1
-        end_num = int(demand_end) - 1 # index 从 0 计数，故 -1
+        demand_start = int(demand_start) - 1
 
-    return start_num, end_num
+    if demand_end == '':
+        demand_end = None
+    elif not demand_end.isdigit():
+        print('ERROR: Wrong Condition!(e.g: 2-7)')
+        sys.exit(1)
+    else:
+        demand_end = int(demand_end)
+
+    return demand_start, demand_end
 
 def processing_pieces(pieces, demand):
-    print('>'*5, 'pieces: ', pieces)
+    print('line: ', pieces)
     start_num, end_num = processing_demand(demand)
-    r_start, r_end =  start_end_num_in_range(pieces, start_num, end_num)
-    processed_pieces = pieces[r_start : slice_add_one(r_end)]
-    
+
+    processed_pieces = pieces[start_num : end_num]
+    print('result: ', end='')
+    print('end_num: ', end_num)
+
     for stuff in processed_pieces:
         print(stuff, end='')
     print('\n')
 
 def cut_text_lists(text_lists, settings):
     mode, demand, divide = settings
-    print('>>mode: ', mode)
 
     for line in text_lists:
-        if line == '\n':
-            continue
         pieces = cut_line_to_pieces(line, divide)
         processing_pieces(pieces, demand)
+
 
 args = argv()
 lines = get_lines(args.content)
